441fe2f96a8fe307a7786a0e8681d26652cb121e
yskn67 <yskn1967@gmail.com>
2023-10-22 03:19:25 +0000

    Cleanup git configuration

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,4 +2,8 @@
 
 set -euxo pipefail
 
+# Git
+cp .devcontainer/.gitconfig ~/.gitconfig
 git config --global --add safe.directory /workspace
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USERNAME
