6f457827f6d73231839c8ccc9920c940205090a3
yskn67 <yskn1967@gmail.com>
2023-10-27 02:02:48 +0000

    Use transformer instead of conformer

--- bin/train.sh
+++ bin/train.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 # python3 src/train.py
 # python3 src/musicvae/pretrain.py name=fourth
-# python3 src/musicvae/pretrain.py name=second_nt train.input_dir="data/preprocess_notranspose"
-python3 src/musicvae/finetune.py name=fifth pretrain_name=fourth
-# python3 src/musicvae/finetune.py name=second_nt pretrain_name=second_nt
+# python3 src/musicvae/finetune.py name=fifth pretrain_name=fourth
+python3 src/melodyfixer/pretrain.py name=second
+python3 src/melodyfixer/finetune.py name=second pretrain_name=second
